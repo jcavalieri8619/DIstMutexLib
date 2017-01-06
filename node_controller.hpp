50af2efc3c9d590fdc960aa3d102c0ccb40994d0
John P Cavalieri <jcavalieri8619@gmail.com>
2017-01-06 12:46:40 -0500

    comments

--- node_controller.hpp
+++ node_controller.hpp
@@ -85,74 +85,90 @@ namespace nodeController
     /**
      * monitors TCP socket for REQUEST messages from remote peer
      * node controllers
-     * @param argument is remoteMonitor::thread_arg
+     * @param argument:  remoteMonitor::thread_arg
      * @return
      */
     void * remote_peer_monitor( void * argument );
 
 
     /**
-     * monitors Unix domain socket at address LocalListensock for REQUEST
-     * messages from local clients
-     * @param argument is localMonitor::thread_arg
+     * monitors Unix domain socket at address LocalListensock_template for
+     * REQUEST messages from local clients
+     * @param argument:  localMonitor::thread_arg
      * @return
      */
     void * local_process_monitor( void* argument );
 
 
     /**
-     * waits for notification that a local client's request has reached
-     * head of request queue and sends PERMISSION GRANTED to that local client
-     * over Unix domain socket LocalSendersock then waits for FINISHED_CS
-     * message from that local client so that it can allow that message
-     * to be popped off request queue.
-     * @param argument
+     * waits for notification from queue_notifier that a local client's
+     * REQUEST has reached head of request queue and sends PERMISSION GRANTED
+     * to that local client over Unix domain socket whose address is contained
+     * within loc_msg field of the message then waits for FINISHED_CS message
+     * from that local client.
+     * once it receives FINISHED_CS, it will allow queue_notifier thread to pop
+     * local client's REQUEST message from head of queue and then pop
+     * deferred REPLY list from same queue.
+     * @param argument thread_arg
      * @return
      */
     void * local_process_sender( void* argument );
 
     /**
-     * all incoming REQUEST messages from local and remote monitor threads are
-     * fed into this thread for time-stamping and routed to appropriate
-     * location--either remote_peer_sender or request_queue_manager
-     * @param argument
+     * all incoming messages from both local and remote monitor threads
+     * are fed into this thread for timestamping and routed to appropriate
+     * location--either remote_peer_sender and request_queue_manager for
+     * incoming local REQUEST messages or just request_queue_manager for
+     * incoming remote REQUEST and REPLY messages
+     * @param argument thread_arg
      * @return
      */
     void * incoming_msg_handler( void* argument );
 
     /**
      * all outgoing REPLYs to remote peers are fed into this thread for
-     * time-stamping and other record keeping then routed to remote_peer_sender
-     * @param argument
+     * time-stamping and other record keeping then routed to remote_peer_sender.
+     * After localSender thread receives FINISHED_CS message from local client, it
+     * will allow queue_notifier to pop all messages whose REPLY was
+     * deferred and each of those messages will be fed into this
+     * thread where it is converted to REPLY message and its timestamp is updated
+     * @param argument thread_arg
      * @return
      */
-    void * outgoing_peerMsg_handler( void* argument );
+    void * outgoing_peer_replyMsg_handler( void* argument );
+
 
     /**
-     * sends REPLY and REQUEST messages out to remote peer node controllers
-     * @param argument
+     * broadcasts outgoing REQUEST messages to all remote peer node controllers
+     * and sends REPLY messages to particular remote peer nodes whose REQUEST
+     * message is ahead of mine on requestQueue
+     * @param argument  thread_arg
      * @return
      */
     void * remote_peer_sender( void* argument );
 
+
     /**
      * enqueues in sorted order all REQUEST messages into appropriate
-     * row of request queue and dequeues any REQUEST messages that have been
+     * row of requestQueue and dequeues any REQUEST messages that have been
      * REPLY'd too
-     * @param argument
+     * @param argument  thread_arg
      * @return
      */
     void * request_queue_manager( void* argument );
 
     /**
-     * examines all rows of request queue to determine whether to notify local
-     * client that it has reached head of queue or to send out REPLY to remote
-     * peer because no local REQUESTS ahead of it.
-     * @param argument
+     * monitors requestQueue row (thread_arg.act, thread_arg.sub_act) to determine
+     * whether to notify local client that it has reached the head of its row or
+     * to send out REPLY to remote peer because no local REQUESTS are ahead of it
+     * @param argument  thread_arg determines which row of requestQueue to monitor
      * @return
      */
     void * request_queue_notifier( void* argument );
 
+    //MessageBuffer's are used as ring buffers inside the node controller for
+    //passing messages between its threads; the number of rows nrows is
+    //determined by the needs of the threads acting on it
     template< size_t nrows, size_t buffsize>
     class MessageBuffer;
 
@@ -160,6 +176,9 @@ namespace nodeController
 
     typedef int32_t logical_time;
 
+    /**
+     * implements Lamport's logical clock
+     */
     class LogicalClock
     {
         logical_time time;
@@ -173,17 +192,36 @@ namespace nodeController
         {
             UNITIALIZED = -1
         };
-        //if msg's timestamp is uninit then apply timestamp
-        //else compare msg's timestamp to local clock and update if needed
+
+        /**
+         * acts on logical_time type, not Message, so pass in the
+         * timestamp field of the message and it will be updated via value-result
+         * argument type. if msg_time is UNINITIALIZED then return current
+         * clock value and update clock, but if msg_time is not UNINITIALIZED
+         * then compare msg_time with current clock value and update clock value
+         * if necessary
+         * @param msg_time  the timestamp field of Message structure
+         */
         void operator()( logical_time& msg_time );
+
+        /**
+         * resets msg_time to UNINTIALIZED so that the associated Message can be
+         * converted from REQUEST to REPLY.  Once timestamp is reset, use
+         * the call operator defined above to get correct value for timestamp
+         * @param msg_time  timestamp field of Message structure
+         */
         void clear_timestamp( logical_time& msg_time );
     };
 
     struct localMessage
     {
-        //these values are obtained from recvfrom in local_process_monitor thread
+        //cliaddr and clilen obtained from recvfrom call in localMonitor thread
+        //and then used by localSender to send messages back to the particular
+        //local client
         struct sockaddr_un cliaddr;
         socklen_t clilen;
+
+        //PID of local client
         pid_t localPID;
 
 
@@ -195,34 +233,47 @@ namespace nodeController
 
     };
 
+
+    /**
+     * message structure passed back and forth between remote-peer
+     * node controllers
+     */
     struct Message
     {
-        //request type
-        //action type (donut flavor)
-        //timestamp
-        //node ID
-        //local message: if message is for local client then will point to
-        //localMessage struct containing structaddr_un for the particular
-        //client process that made the request so that we can send message to him
-        //when its his time to enter C.S.
+
+        //each message sent between node controllers is either REQUEST or REPLY
         MESSAGE_T type;
+
+        //if type if REQUEST then this indicates that node sending message
+        //is requesting (Action act, subAction sub_act) e.g (CONSUME, FLAVOR_1);
+        //if type is REPLY then this message is replying to a peer node that
+        //previously requested (act, sub_act)
         Action act;
+
+        //which "flavor" to produce or consume where flavor can represent any
+        //object that client wishes to consume or produce
         subAction sub_act;
 
         //node_ID is overloaded so that when a message is sent it indicates
         //the receiving node (ME IF SENDING TO ME) but when a message arrives, the node_ID is
         //overwritten to indicate the sending node (PERSON THAT SENT TO ME)
         uint32_t node_ID;
-        logical_time time_stamp; //= logicalClock::uninitialized
-        uint32_t rcvdReplies; // = 0
-        localMessage* loc_msg; // = nullptr
 
-        //had to remove in-struct initializers so that I could use
-        //aggregate initialization in testing but need to replace
-        //these initializations somehow (just wait until testing complete?)
+        //implemented as Lamport's logical clock
+        logical_time time_stamp;
 
+        //bit array where each bit indicates whether that node sent a REPLY
+        uint32_t rcvdReplies;
+
+        //if message is associated with a local client request then this pointer
+        //will be non-NULL and will point to a structure needed to communicate
+        //with local client
+        localMessage* loc_msg;
+
+
+        //both lt and eq operators induce total ordering on message in
+        //requestQueue
         bool operator<(const Message&) const;
-
         bool operator==(const Message&) const;
 
 
@@ -232,7 +283,14 @@ namespace nodeController
 
     };
 
+    /**
+     * updates the bit array Message.rcvdReplies to flip the bit on for the
+     * argument node_ID
+     * @param node_ID the ID of the node that sent a REPLY message
+     * @param reply_array bit array stored in Message structure
+     */
     void update_rcvd_replies_array( unsigned node_ID, unsigned& reply_array );
+
 
 
     namespace remoteSender
@@ -242,33 +300,36 @@ namespace nodeController
         {
             static constexpr size_t incoming_NROWS = 1;
             static constexpr size_t incoming_BUFFSIZE = MSG_BUFF_SIZE;
+            //ring buffer that contains outgoing REQUEST and REPLY messages
+            //that were produced by incoming_msg_handler thread and
+            //outgoing_reply_handler thread
             MessageBuffer<incoming_NROWS, incoming_BUFFSIZE> *
             in_buffer;
         };
     }
 
-    //need a queue_monitor for Prod/Cons every flavor and each queue_monitor
-    //is paired with a local_sender so that the queue_monitors outgoing buffer
-    //is the local_sender's incoming buffer.
 
     namespace localSender
     {
 
         struct thread_arg
         {
-            //need someway to create unique string path for unix domain
-            //socket for each local_sender thread--perhaps use strncpy and
-            //append the Action and subAction of the associated queue_monitor
 
-            //same act,sub_act passed into associated queue_monitor
+            //same act,sub_act pair passed to the associated queue_notifier
+            //so that the queue_notifier for (CONSUME,FLAVOR_1) indicates that
+            //a local client reached head of queue, it will enqueue a message
+            //on in_buffer of localSender for (CONSUME,Flavor_1). This setup
+            //allows local clients to produce and consume the same object
+            //concurrently but prevents 2 producers / 2 consumers from
+            //producing / consuming same object concurrently
             Action act;
             subAction sub_act;
 
 
-            //all local senders will get same msg buffer arg but
-            //each will use their own row of buffer.
+
             static constexpr size_t incoming_NROWS = 2 * NUMFLAVORS;
             static constexpr size_t incoming_BUFFSIZE = MSG_BUFF_SIZE;
+
             MessageBuffer<incoming_NROWS, incoming_BUFFSIZE> *
                     in_buffer = nullptr;
         };
@@ -281,16 +342,16 @@ namespace nodeController
 
         struct thread_arg
         {
-            //out_buffer to remote_peer_sender threads
+            //out_buffer is the ring buffer that contains all outgoing messages
+            //to remote peers
             static constexpr size_t outPeer_NROWS = 1;
             static constexpr size_t outPeer_BUFFSIZE = MSG_BUFF_SIZE;
             MessageBuffer<outPeer_NROWS, outPeer_BUFFSIZE> *
             out_buffer;
 
 
-            //in_buffer is paired with queue_monitor threads
-            //so that multiple queue_monitor threads will produce messages
-            //into this buffer
+            //in_buffer is the ring buffer that all queue_notifier threads will
+            //enqueue messages into that need to be REPLYied too
             static constexpr size_t incoming_NROWS = 1;
             static constexpr size_t incoming_BUFFSIZE = MSG_BUFF_SIZE;
             MessageBuffer<incoming_NROWS, incoming_BUFFSIZE> *
